d36f37a8e060ee84f489ab83b314faadac8575ed
Eloy Martin fernandez <elmartin@c1r10s4.42madrid.com>
2021-04-12 20:37:42 +0200

    ft_memcpy.c

--- Libft/sources/ft_memcpy.c
+++ Libft/sources/ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: elmartin <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/12 18:28:09 by elmartin          #+#    #+#             */
-/*   Updated: 2021/04/12 19:43:32 by elmartin         ###   ########.fr       */
+/*   Updated: 2021/04/12 20:37:04 by elmartin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,17 @@
 
 void	*ft_memcpy(void *dst, const void *src, size_t n)
 {
-	size_t i;
+	size_t	i;
+	char	*sd;
+	char	*ss;
 
 	i = 0;
-	while(dst[i])
+	ss = (char *)src;
+	sd = (char *)dst;
+	while (n--)
 	{
+		sd[i] = ss[i];
 		i++;
 	}
-	
+	return (dst);
 }
